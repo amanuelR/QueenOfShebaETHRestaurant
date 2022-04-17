dabd7acb87b76e737a1ffe7331c56e645a2af63c
amunigk <gebretsadikamanuel@gmail.com>
2022-04-16 17:11:41 -0700

    Working on JS

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-<script type="module">
-            $(function () {
-                $("#options").change(function () {
-                    if ($(this).val() == "option_one") {
-                        $("#lunch").show();
-                        $("#breakfast").hide();
-                    }
 
-                    else if ($(this).val() == "option_two") {
-                        $("#lunch").hide();
-                        $("#breakfast").show();
-                    }
-                    else {
-                        $("#lunch").show();
-                        $("#breakfast").hide();
-                    }
-                });
-            });
-</script>
+$(function () {
+    $("#options").change(function () {
+        if ($(this).val() == "option_one") {
+            $("#lunch").show();
+            $("#breakfast").hide();
+        }
+
+        else if ($(this).val() == "option_two") {
+            $("#lunch").hide();
+            $("#breakfast").show();
+        }
+        else {
+            $("#lunch").show();
+            $("#breakfast").hide();
+        }
+    });
+});
